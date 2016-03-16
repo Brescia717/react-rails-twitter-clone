d4d7ac5d60014268e013a457647bbd53864869fa
Paul Brescia <brescia717@gmail.com>
2016-03-16 18:30:53 -0400

    1. Created Tweet 2. Tweet is now included in TweetsList 3. Modified main.jsx to reflect changes to TweetsList

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -3,12 +3,18 @@ import Greet from './greet';
 import TweetBox from "./components/TweetBox"
 import TweetsList from "./components/TweetsList"
 
+let mockTweets = [
+  { id: 1, name: 'Paul Brescia', body: 'My #FirstTweet' },
+  { id: 2, name: 'Paul Brescia', body: 'My #SecondTweet' },
+  { id: 3, name: 'Paul Brescia', body: 'My #ThirdTweet' }
+];
+
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
         <TweetBox />
-        <TweetsList />
+        <TweetsList tweets={ mockTweets }/>
       </div>
     );
   }
