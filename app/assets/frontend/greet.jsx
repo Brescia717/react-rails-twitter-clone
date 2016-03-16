0d4091016919c60e119d0a9e58397d28227fa3c7
Paul Brescia <brescia717@gmail.com>
2016-03-16 17:22:48 -0400

    Moved main.jsx and greet.jsx to assets/frontend

---
+++ app/assets/frontend/greet.jsx
@@ -0,0 +1,6 @@
+// This file uses jsx syntax
+export default class Greet extends React.Component {
+  render() {
+    return <h2>Hello Greet</h2>;
+  }
+}
---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,19 @@
+// This file uses jsx syntax
+import Greet from './greet';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <Greet />
+    );
+  }
+}
+
+let documentReady = () => {
+  React.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
