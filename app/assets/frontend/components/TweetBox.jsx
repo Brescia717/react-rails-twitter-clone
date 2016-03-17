ef05dc930d25e4fd3442513ae005860e42e59b58
Paul Brescia <brescia717@gmail.com>
2016-03-17 10:37:41 -0400

    Added Tweeting functionality

--- app/assets/frontend/components/TweetBox.jsx
+++ app/assets/frontend/components/TweetBox.jsx
@@ -1,12 +1,17 @@
 export default class TweetBox extends React.Component {
+  sendTweet(event) {
+    event.preventDefault();
+    this.props.sendTweet(React.findDOMNode(this.refs.tweetTextArea).value);
+    React.findDOMNode(this.refs.tweetTextArea).value = '';
+  }
   render() {
     return (
       <div className="row">
-        <form>
+        <form onSubmit={ this.sendTweet.bind(this) }>
           <div className="input-field">
-            <textarea className="materialize-textarea" />
+            <textarea ref="tweetTextArea" className="materialize-textarea" />
             <label>What's happening?</label>
-            <button className="btn right">Tweet</button>
+            <button type="submit" className="btn right">Tweet</button>
           </div>
         </form>
       </div>
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -3,18 +3,24 @@ import Greet from './greet';
 import TweetBox from "./components/TweetBox"
 import TweetsList from "./components/TweetsList"
 
-let mockTweets = [
-  { id: 1, name: 'Paul Brescia', body: 'My #FirstTweet' },
-  { id: 2, name: 'Paul Brescia', body: 'My #SecondTweet' },
-  { id: 3, name: 'Paul Brescia', body: 'My #ThirdTweet' }
-];
+
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tweetsList: [] };
+  }
+  addTweet(tweetToAdd) {
+    let newTweetsList = this.state.tweetsList;
+    newTweetsList.unshift({ id: Date.now(), name: 'Guest', body: tweetToAdd });
+
+    this.setState({ tweetsList: newTweetsList });
+  }
   render() {
     return (
       <div className="container">
-        <TweetBox />
-        <TweetsList tweets={ mockTweets }/>
+        <TweetBox sendTweet={ this.addTweet.bind(this) }/>
+        <TweetsList tweets={ this.state.tweetsList }/>
       </div>
     );
   }
