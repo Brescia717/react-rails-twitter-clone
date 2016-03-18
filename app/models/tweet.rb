edeac1e3a871953ab4340c48ff188efe05fc13c4
Paul Brescia <brescia717@gmail.com>
2016-03-18 12:28:38 -0400

    Set up user/tweet association

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,14 @@
 class Tweet < ActiveRecord::Base
+  belongs_to :user
+
+  # The as_json method functions as it normally would, but we
+  # add the `super(methods: [:name])` to include the name 
+  # method defined in this model, when the json is returned.
+  def as_json(options={})
+    super(methods: [:name])
+  end
+
+  def name
+    user.display_name
+  end
 end
