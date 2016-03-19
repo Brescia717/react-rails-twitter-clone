efd514d3f9912a975c6b588f48200a87b24facfd
Paul Brescia <brescia717@gmail.com>
2016-03-19 15:16:15 -0400

    Updated main.jsx to reflect changes to having moved the code to seperate component locations/directories

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,37 +1,40 @@
 // This file uses jsx syntax
-import Greet from './greet';
 import TweetBox from "./components/TweetBox"
 import TweetsList from "./components/TweetsList"
+import TweetStore from "./stores/TweetStore"
+import TweetActions from "./actions/TweetActions"
+
+let getAppState = () => {
+  return { tweetsList: TweetStore.getAll() };
+}
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tweetsList: [] };
-  }
-  formattedTweets(tweetsList) {
-    let formattedList = tweetsList.map(tweet => {
-      tweet.formattedDate = moment(tweet.created_at).fromNow();
-      return tweet;
-    })
-    return {
-      tweetsList: formattedList
-    };
+    this.state = getAppState();
+    this._onChange = this._onChange.bind(this);
   }
   addTweet(tweetToAdd) {
     // Tweet is added to the backend, before changes are made on the frontend.
-    $.post("/tweets", { body: tweetToAdd })
-    .success( savedTweet => {
-      let newTweetsList = this.state.tweetsList;
-      newTweetsList.unshift(savedTweet);
-
-      this.setState(this.formattedTweets(newTweetsList));
-    })
-    .error( error => console.log(error));
+    // $.post("/tweets", { body: tweetToAdd })
+    // .success( savedTweet => {
+    //   let newTweetsList = this.state.tweetsList;
+    //   newTweetsList.unshift(savedTweet);
+    //
+    //   this.setState(this.formattedTweets(newTweetsList));
+    // })
+    // .error( error => console.log(error));
   }
   componentDidMount() {
-    $.ajax("/tweets")
-    .success(data => this.setState(this.formattedTweets(data)))
-    .error(error => console.log(error));
+    TweetActions.getAllTweets();
+    TweetStore.addChangeListener(this._onChange);
+  }
+  componentWillUnmount() {
+    TweetStore.removeChangeListener(this._onChange);
+  }
+  _onChange() {
+    console.log(5, "Main._onChange");
+    this.setState(getAppState());
   }
   render() {
     return (
