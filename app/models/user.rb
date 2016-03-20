6d1bffb29d3c3b375db74798d7c531d1779a62e6
Paul Brescia <brescia717@gmail.com>
2016-03-20 15:25:17 -0400

    Modified sql queries and moved them into respective models

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,9 +21,9 @@ class User < ActiveRecord::Base
 
   def self.who_to_follow(current_user_id)
     where(["id != :current_user_id AND NOT EXISTS (
-        SELECT 1 FROM followers
-        WHERE user_id = users.id
-        AND followed_by = :current_user_id
+      SELECT 1 FROM followers
+      WHERE user_id = users.id
+      AND followed_by = :current_user_id
       )", { current_user_id: current_user_id }])
     .order("random()").all
   end
